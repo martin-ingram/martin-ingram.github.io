d22ef4602e1daa371cd8b33cb70ca5ce560709bc
Martin Ingram <martin.ingram@cabinetoffice.gov.uk>
2023-11-23 15:47:33 +0000

    made data more realistic with trends over time in FTE

--- orgs_test/2023-11-21 Generate Synthetic Orgs Data.R
+++ orgs_test/2023-11-21 Generate Synthetic Orgs Data.R
@@ -4,15 +4,17 @@
 library(tidyverse)
 library(tidylog)
 
+# Set random seed or reproducibility
+set.seed(123) 
+
 # List of fictional organisations
 orgs_list <- 
-    readxl::read_xlsx("orgs_test/2023-11-21 fictional_orgs_list.xlsx")
+    readxl::read_xlsx("orgs_test/2023-11-21 fictional_orgs_list.xlsx") %>% 
+    mutate(fte = rnorm(n = nrow(.), mean = 1500, sd = 1050)) %>% 
+    mutate(fte = case_when(.default = fte, fte < 0 ~ 0))
 
 # years of data we want to generate
 years <- 2010:2023
-
-# Set random seed or reproducibility
-set.seed(123) 
 
 # build time series data
 org_time_series <- replicate(n = length(years), orgs_list, 
@@ -38,9 +40,6 @@ output_org_time_series <- org_time_series %>%
                                       pattern = ",|'|’"))) %>% 
     mutate(content_type = recode(content_type, 
                 "Comic/Video Game" = "Comic")) %>% 
-    mutate(fte = sample(400:1000,
-                        nrow(org_time_series),
-                        replace = TRUE)) %>% 
     mutate(running_costs = fte * sample(250:35000,
                     nrow(org_time_series), replace = TRUE)) %>% 
     mutate(investment_costs = running_costs * 0.1046) %>% 
@@ -54,8 +53,37 @@ output_org_time_series <- org_time_series %>%
     mutate(noise = rnorm(n = nrow(org_time_series),
                          mean = mean(funding),
                          sd = sd(funding)) / 10) %>% 
+    mutate(running_costs = running_costs + noise * 4) %>% 
     mutate(funding = funding + noise) %>% 
-    select(-noise)
+    mutate(noise_fte = rnorm(n = nrow(org_time_series),
+                         mean = mean(fte),
+                         sd = sd(fte)) / 20) %>% 
+    group_by(organisation) %>% 
+    mutate(noise_fte = cumsum(noise_fte)) %>% 
+    mutate(fte = case_when(.default = fte + noise_fte, 
+            grepl(x = organisation,
+                 pattern = "^F|^H")  ~ fte + (noise_fte * 2),
+            grepl(x = organisation,
+                  pattern = "^W")  ~ fte - noise_fte,
+            grepl(x = organisation,
+                  pattern = "^L")  ~ fte - (noise_fte*2),
+            time %in% c('2016', "2020") & fte > 2000 & 
+                grepl(x = organisation,
+                      pattern = "^A|^S|^M|^C|^O")  ~ fte - noise_fte *
+                               runif(n = 1, min = 0.04, max = 2))) %>% 
+    mutate(fte = case_when(.default = fte,
+            fte < 0 ~ 0)) %>% 
+    # mutate(fte = ) %>% 
+    select(-starts_with('noise')) %>%
+    mutate(across(c("fte", 
+                    "running_costs",
+                    "investment_costs",
+                    "funding"), \(x) round(x)))
 
 write_csv(output_org_time_series,
-          file = "orgs_test/2023-11-21 - fictional_orgs_time_series.csv")
+          file = "orgs_test/slides/2023-11-21 - fictional_orgs_time_series.csv")
+
+write_csv(output_org_time_series,
+          file = "orgs_test/report/2023-11-21 - fictional_orgs_time_series.csv")
+
+message(paste0(Sys.time(), " - synthetic data rebuilt"))
