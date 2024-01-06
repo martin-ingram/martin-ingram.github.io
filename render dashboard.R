925ad52d9c94d2ba1e3831c05712a58c773adca4
Martin Ingram <martin.ingram@cabinetoffice.gov.uk>
2024-01-06 20:07:58 +0000

    flexdashboard test (publishes to shinyapps.io)

---
+++ render dashboard.R
@@ -0,0 +1,10 @@
+## Render Data Explorer
+
+library(rsconnect)
+
+# authenticate rseconnect
+source("~/Martin Ingram R Work/rsconnect/rsconnect.R")
+
+# deploy App
+deployApp(appDir = 'flexdashboard test/')
+
